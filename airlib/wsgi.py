a35ceb64e55f4be8beaf3db0b6af499bbd9d8905
novrizair <novrizal.airsyahputra@ui.ac.id>
2023-09-10 11:21:21 +0700

    bismillah proyek nabi adam

---
+++ airlib/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for airlib project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airlib.settings')
+
+application = get_wsgi_application()
